87aeb19b2f925a4d4c77f0ecef59e81d9ac1c826
林子傑 <jeffreyjeffreylin98@gmail.com>
2022-01-13 23:34:22 -0600

    like/unlike

--- src/App.js
+++ src/App.js
@@ -63,6 +63,7 @@ function App() {
 
         Results
         <Container />
+
       </Page>
     </AppProvider>
   );
--- src/components/gallery.js
+++ src/components/gallery.js
@@ -1,9 +1,11 @@
-import { Image, Card } from '@shopify/polaris';
+import React, { useState } from "react";
+import { Image, Card, Button } from '@shopify/polaris';
 
 const Gallery = props => {
     const results = props.data;
+    const [likedImages, setLikedImages] = useState(new Set());
+
     let images;
-    // map variables to each item in fetched image array and return image component
     if (results.length > 0) {
         images = results.map(image => {
             let id = image.id;
@@ -18,7 +20,23 @@ const Gallery = props => {
                 <p> Rover: {rover} </p>
                 <p> Camera: {camera} </p>
                 <p> Date: {date} </p>
-            </Card>;
+                <Button id={id} primary={likedImages.has(id)} onClick={() => {
+                    if (likedImages.has(id)) {
+                        setLikedImages(prev => {
+                            const cur = new Set(prev);
+                            cur.delete(id);
+                            return cur;
+                        });
+                    } else {
+                        setLikedImages(prev => {
+                            const cur = new Set(prev);
+                            cur.add(id)
+                            return cur;
+                        });
+                    };
+                    console.log(likedImages);
+                }}> Like </Button>
+            </Card >;
         });
     }
     return <div> {images} </div>;
